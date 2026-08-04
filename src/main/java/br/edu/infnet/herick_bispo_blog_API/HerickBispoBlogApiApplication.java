b64628455955be54addb560b32996cd4b3003511
Herick Bispo <hbispo.dev@gmail.com>
2026-08-04 11:52:29 -0300

    first commit

---
+++ src/main/java/br/edu/infnet/herick_bispo_blog_API/HerickBispoBlogApiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.herick_bispo_blog_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerickBispoBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerickBispoBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/infnet/herick_bispo_blog_API/HerickBispoBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.infnet.herick_bispo_blog_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerickBispoBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
